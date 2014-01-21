9136447613919141a181f3ae6afdb677792a051a
truewalrus <unbreakablescorpions@gmail.com>
2014-01-21 15:12:08 -0800

    merge conflicts

--- personality-test-help.php
+++ personality-test-help.php
@@ -1,3 +1,24 @@
 <div class = "wrap">
 	<h2>Help</h2>
+	Table of Contents:
+	<ol>
+		<li>Quick Introduction</li>
+		<li>Creating a Quiz</li>
+		<li>Adding Results</li>
+		<li>Adding Questions</li>
+		<li>The Results Page</li>
+		<li>Adding the Quiz to Your Website</li>
+		<li>Finishing Touches</li>
+	</ol>
+	
+	<ol class = "ptest-help-ol">
+		<li>
+			Quick Introduction
+			<p class = "ptest-help-ol-p">Welcome to Ptest, an easy to use interest and personality test creator. Select <a href = "<?php echo add_query_arg(array('page' => 'Quiz'), admin_url('admin.php'))?>" >Quiz List</a> if you want to get started right away. If not, continue to <a href = "#create">Creating a Quiz</a> to continue with this tutorial</p>
+		</li>
+		<li>
+			Creating a Quiz
+			<p class = "ptest-help-ol-p">Creating a quiz is simple. First, select <a href = "<?php echo add_query_arg(array('page' => 'Quiz'), admin_url('admin.php'))?>" >Quiz List</a> from the dropdown in the admin menu, or click the link. You will be taken to the quiz list screen. <img src = "<?php echo plugin_dir_path( __FILE__) . "img/empty-quiz-list.png" ?> ">
+			
+		</li>
 </div>
